85437810cf56ad8722a061917cd253dcbb0e838b
Chloekkk <skdusdl8804@gmail.com>
2021-08-16 23:25:25 +0900

    ex03 : fix code align

--- js00/ex03/dynamicType.js
+++ js00/ex03/dynamicType.js
@@ -1,36 +1,37 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   dynamicType.js                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/16 23:19:47 by nkim              #+#    #+#             */
-/*   Updated: 2021/08/16 23:19:49 by nkim             ###   ########.fr       */
-/*                                                                            */
+/*  */
+/* :::      :::::::: */
+/* dynamicType.js                                     :+:      :+:    :+: */
+/* +:+ +:+         +:+ */
+/* By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+ */
+/* +#+#+#+#+#+   +#+ */
+/* Created: 2021/08/16 23:19:47 by nkim              #+#    #+# */
+/* Updated: 2021/08/16 23:19:49 by nkim             ###   ########.fr */
+/*  */
 /* ************************************************************************** */
 
 const dynamicType = {
     init_val: undefined,
-	ch_val: undefined,
-    put : (init_val) => {
-		if (typeof init_val === "number")
-			this.init_val = init_val;
-		else
-			console.log("WrongInputError: input type is not a Number");
-	},
-    change : (type) => {
-		if (type === "String") {
-			ch_val = "\"" + String(this.init_val) + "\"";
-		} else if (type === "Number") {
-			ch_val = Number(this.init_val);
-		} else if (type === "Array") {
-			ch_val = [this.init_val];
-		} else if (type === "Object") {
-			ch_val = Object(this.init_val);
-		}
-	},
-    printType : () => {
-		console.log(ch_val);
-	}
+    ch_val: undefined,
+    put: (init_val) => {
+        if (typeof init_val === "number") 
+            this.init_val = init_val;
+        else 
+            console.log("WrongInputError: input type is not a Number");
+        }
+    ,
+    change: (type) => {
+        if (type === "String") {
+            ch_val = "\"" + String(this.init_val) + "\"";
+        } else if (type === "Number") {
+            ch_val = Number(this.init_val);
+        } else if (type === "Array") {
+            ch_val = [this.init_val];
+        } else if (type === "Object") {
+            ch_val = Object(this.init_val);
+        }
+    },
+    printType: () => {
+        console.log(ch_val);
+    }
 }

9397622813f1aaf5ae02c98bf339c96f854ce372
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-16 17:48:58 +0900

    ✅ Pass test of exid-method-chaining.js

---
+++ js00/ex04/exid-method-chaining.js
@@ -0,0 +1,23 @@
+const exidMethodChaining = {
+  count: 40,
+  up() {
+    this.count += 1;
+    return this;
+  },
+  down() {
+    this.count -= 1;
+    return this;
+  },
+  showCount() {
+    console.log(this.count);
+  },
+};
+
+exidMethodChaining.up().down().up().up()
+  .down()
+  .up()
+  .down()
+  .up()
+  .up()
+  .down()
+  .showCount();

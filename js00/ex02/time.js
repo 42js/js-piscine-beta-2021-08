b725ad47306391298423f594cf8c660fb4fece06
Kimhan-nah <skqkdldhf98@naver.com>
2021-08-16 18:07:02 +0900

    ex02

---
+++ js00/ex02/time.js
@@ -0,0 +1,14 @@
+const month = {month : 7};
+let day = 13;
+
+const timeWarp = (newMonth, newDay) => {
+    month.month = newMonth;
+    day = newDay;
+}
+
+// timeWarp(5,10);
+// console.log(month);
+// console.log(day);
+// timeWarp(11,20);
+// console.log(month);
+// console.log(day);

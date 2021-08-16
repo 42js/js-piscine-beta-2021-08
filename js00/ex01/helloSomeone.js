7fa30a9c2658428f1934692114922b698900faef
Woohyun Park <woohyunpark.mail@gmail.com>
2021-08-16 12:19:00 +0900

    FIX, fix if statements

--- js00/ex01/helloSomeone.js
+++ js00/ex01/helloSomeone.js
@@ -3,10 +3,10 @@ function helloSomeone(str){
         console.log("Who are you?");
     } else if(str === null){
         console.log("I am null and void")
-    } else if(str/1 === str){
-        str/1 > 0 ? console.log(`My age is ${str}`) : console.log("I am Benjamin Button!");
     } else if(str === undefined){
         console.log("Nobody can define me!");
+    } else if(typeof str === "number" && !isNaN(str)){
+        str/1 > 0 ? console.log(`My age is ${str}`) : console.log("I am Benjamin Button!");
     } else if(isNaN(str)){
         console.log("Age is just a number");
     } else {

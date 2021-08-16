8449b795ec0c3ea01a5a10ff61d5444e81222347
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-16 16:41:06 +0900

    ♻♻️ Refactor code

--- js00/ex01/helloSomeone.js
+++ js00/ex01/helloSomeone.js
@@ -1,16 +1,22 @@
-function helloSomeone(arg) {
-  if (typeof arg === 'string') {
-    if (arg.length) console.log(`Hello ${arg}!`);
-    else console.log('Who are you?');
-  } else if (typeof arg === 'object' && arg == null) {
-    console.log('I am null and void');
-  } else if (typeof arg === 'number') {
-    if (Number.isNaN(arg)) console.log('Age is just a number');
-    else if (arg > 0) console.log(`My age is ${arg}`);
-    else console.log('I am Benjamin Button!');
-  } else if (typeof arg === 'undefined') {
-    console.log('Nobody can define me!');
+export default function helloSomeone(arg) {
+  switch (typeof arg) {
+    case 'string':
+      if (arg.length) console.log(`Hello ${arg}!`);
+      else console.log('Who are you?');
+      break;
+    case 'object':
+      if (arg == null) console.log('I am null and void');
+      break;
+    case 'number':
+      if (Number.isNaN(arg)) console.log('Age is just a number');
+      else if (arg > 0) console.log(`My age is ${arg}`);
+      else console.log('I am Benjamin Button!');
+      break;
+    case 'undefined':
+      console.log('Nobody can define me!');
+      break;
+    default:
+      console.log('Nothing....');
+      break;
   }
 }
-
-export { helloSomeone as default };

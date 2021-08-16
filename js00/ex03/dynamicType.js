1362e7df685650fa91f260e2e78087687e8405d6
강명구 <mg5566@hanmail.net>
2021-08-16 19:08:47 +0900

    update comments

--- js00/ex03/dynamicType.js
+++ js00/ex03/dynamicType.js
@@ -1,26 +1,30 @@
 function to_string(object, value) {
-	if (object.type != "String") {
+	// 객체의 type 이 change() 의 인자와 다른 경우
+	if (object.type != value) {
 		object.print_val = "\"" + object.val + "\"";
 		object.type = value;
 	}
 }
 
 function to_number(object, value) {
-	if (object.type != "Number") {
+	// 객체의 type 이 change() 의 인자와 다른 경우
+	if (object.type != value) {
 		object.print_val = object.val;
 		object.type = value
 	}
 }
 
 function to_object(object, value) {
-	if (object.type != "Object") {
+	// 객체의 type 이 change() 의 인자와 다른 경우
+	if (object.type != value) {
 		object.print_val = "{" + object.val + "}";
 		object.type = value;
 	}
 }
 
 function to_array(object, value) {
-	if (object.type != "Array") {
+	// 객체의 type 이 change() 의 인자와 다른 경우
+	if (object.type != value) {
 		object.print_val = "[" + object.val + "]";
 		object.type = value;
 	}
@@ -29,28 +33,28 @@ function to_array(object, value) {
 const dynamicType = {
 	val: undefined,  // 진짜 값
 	print_val: undefined,  // 출력용
-	type: undefined,
+	type: undefined,  // type 을 저장하는 변수
 	put: function (value) {
 		this.val = value;
 		this.print_val = this.val;  // 안해도 된다.
 	},
 	change: function (value) {
 		if (value == "String") {
-			to_string(this, value);
+			to_string(this, value);  // string format 으로 변환시킨다.
 		} else if (value == "Number") {
-			to_number(this, value);
+			to_number(this, value);  // number format 으로 변환시킨다.
 		} else if (value == "Object") {
-			to_object(this);
+			to_object(this);  // object format 으로 변환시킨다.
 		} else if (value == "Array") {
-			to_array(this);
+			to_array(this);  // array format 으로 변환시킨다.
 		}
 	},
 	printType: function (value) {
-		console.log("- " + this.print_val);
+		console.log("- " + this.print_val);  // "- 42" 이런 format 으로 출력
 	}
 }
 
-
+/* test
 type = dynamicType;
 type.put(42);
 type.change("String");
@@ -67,3 +71,10 @@ type.printType();
 type.put(1);
 type.change("Number");
 type.printType();
+
+type.change("Object");
+type.printType();
+
+type.change("Array");
+type.printType();
+*/

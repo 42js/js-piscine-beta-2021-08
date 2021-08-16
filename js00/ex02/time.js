4a5e15339d4ae496592b46094a406bcb397ff75e
배윤희 <gogo@baeyunhuiui-MacBook-Pro.local>
2021-08-16 23:24:36 +0900

    asdf'

--- js00/ex02/time.js
+++ js00/ex02/time.js
@@ -1,12 +1,7 @@
-const month = 7 ;
+const month = { change : 7 };
 let day = 13;
 
 const timeWarp = (newMonth, newDay) => {
-  month = newMonth;
+  month.change = newMonth;
   day = newDay;
 }
-console.log(`month = ${month}, day = ${day}`);
-timeWarp(5,10);
-console.log(`month = ${month}, day = ${day}`);
-timeWarp(11,20);
-console.log(`month = ${month}, day = ${day}`);

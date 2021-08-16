ce08613757e43e01542c0417ddc6a36217e01431
강명구 <mg5566@hanmail.net>
2021-08-16 19:03:05 +0900

    solve exid.js

---
+++ js00/ex04/exid-method-chaining.js
@@ -0,0 +1,11 @@
+const exidMethodChaining = {
+	count: 0,
+	up() {
+	},
+	down() {
+	},
+	showCount() {
+	},
+};
+
+exidMethodChaining.up().down().up().up().down().up().down().up().up().down().showCount();

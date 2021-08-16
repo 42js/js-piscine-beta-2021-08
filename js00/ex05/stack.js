51784c78d258c7562d533d4b7f7e1193b777cd77
강명구 <mg5566@hanmail.net>
2021-08-16 20:14:56 +0900

    complete test

--- js00/ex05/stack.js
+++ js00/ex05/stack.js
@@ -9,30 +9,73 @@ function stackEmpty(stack) {
 }
 
 function stackPush(stack, data) {
-	var index = 0;
-	for (index in stack.arr) {
-		// 아무것도 안함... stack 뒤로 달림.
+	var index;
+	for (index = 0; stack.arr[index]; ++index) {
+		// console.log("push horse goes round " + index);
 	}
+	// console.log("push index: " + index);
 	stack.arr[index] = data;
 }
 
 function stackPeek(stack) {
-	if (stack.arr[0])
-		return (stack.arr);
+	var index;
+	for (index = 0; stack.arr[index]; ++index) {
+		// console.log("peek horse goes round " + index);
+	}
+	return (stack.arr[index - 1]);
 }
 
 function stackPop(stack) {
 	if (!stack.arr[0])  // 없으면 동작 안함...
 		return;
 	// arr[0] 를 arr[1] 로 덮어쓴다.
-	var index = 1;
-	for (index in stack.arr)
-		stack.arr[index - 1] = stack.arr[index];
+	var index;
+	for (index = 1; stack.arr[index]; ++index) {
+		// console.log("pop horse goes round " + index);
+	}
+	stack.arr[index - 1] = undefined;
+	// console.log("pop index: " + index);
 }
 
-stack = stackCreate();
+/*
+// test
+const stack = stackCreate();
+
+// test empty
 var is_empty = stackEmpty(stack);
 if (is_empty)
 	console.log("stack is empty");
 else
 	console.log("stack is not empty");
+// test empty peek
+var temp = stackPeek(stack);
+console.log("Peek is : " + temp);
+
+// test push
+console.log("===push 10===");
+stackPush(stack, 10);
+// test peek
+temp = stackPeek(stack);
+console.log("Peek is : " + temp);
+// test empty
+is_empty = stackEmpty(stack);
+if (is_empty)
+	console.log("stack is empty");
+else
+	console.log("stack is not empty");
+
+
+// test push
+console.log("===push 42===");
+stackPush(stack, 42);
+// test peek
+temp = stackPeek(stack);
+console.log("Peek is : " + temp);
+
+
+// test pop
+stackPop(stack);
+// test peek
+temp = stackPeek(stack);
+console.log("Peek is : " + temp);
+*/

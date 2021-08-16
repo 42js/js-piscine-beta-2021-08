f128f53cdaeed0bcb736e1a11fa3c29be834439f
jinlee <39769058+jinlee0816@users.noreply.github.com>
2021-08-16 20:58:17 +0900

    My Name is -> Hello

--- js00/ex01/helloSomeone.js
+++ js00/ex01/helloSomeone.js
@@ -1,22 +1,22 @@
 function is_number(input) {
-	var n = /^\d+$/;
-	return n.test(input);
+        var n = /^\d+$/;
+        return n.test(input);
 }
 
 export default (input) => {
-	if (typeof input == 'number') {
-		if (input > 0)
-			return console.log("My age is " + input);
-		else if (input <= 0 )
-			return console.log("I am Benjamin Button!");
-		else if (is_number(input) == false)
+        if (typeof input == 'number') {
+                if (input > 0)
+                        return console.log("My age is " + input);
+                else if (input <= 0 )
+                        return console.log("I am Benjamin Button!");
+                else if (is_number(input) == false)
                         return console.log("Age is just a number")
-	} else if (typeof input == 'string') {
-		if (input == '')
-			return console.log("Who are you?");
-		return console.log("My name is " + input);
-	} else if (input === null)
-		return console.log("I am null and void");
-	else if (input === undefined)
-		return console.log("Nobody can define me!");
+        } else if (typeof input == 'string') {
+                if (input == '')
+                        return console.log("Who are you?");
+                return console.log("Hello " + input + "!");
+        } else if (input === null)
+                return console.log("I am null and void");
+        else if (input === undefined)
+                return console.log("Nobody can define me!");
 }

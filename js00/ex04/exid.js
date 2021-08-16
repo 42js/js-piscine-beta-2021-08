1ef03bb0cd8bf63950d394ba3ac89fda4fbf8ef9
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-16 17:45:32 +0900

    ✅ Pass test

---
+++ js00/ex04/exid.js
@@ -0,0 +1,25 @@
+// exid.js
+const exid = {
+  count: 40,
+  up() {
+    this.count += 1;
+  },
+  down() {
+    this.count -= 1;
+  },
+  showCount() {
+    console.log(this.count);
+  },
+};
+
+exid.up();
+exid.down();
+exid.up();
+exid.up();
+exid.down();
+exid.up();
+exid.down();
+exid.up();
+exid.up();
+exid.down();
+exid.showCount();

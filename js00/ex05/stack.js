96ecdea8276306d5345e6067596ad43df1049a46
Sungyong Cho <to.sungyongcho@gmail.com>
2021-08-16 21:15:27 +0900

    저는 이정도 선에서...

--- js00/ex05/stack.js
+++ js00/ex05/stack.js
@@ -3,10 +3,24 @@ const stackCreate = () => ({
 });
 
 function stackEmpty(stack) {
+  if (
+    stack === undefined ||
+    stack === null ||
+    !(typeof stack === 'object' && stack.arr.constructor === Array)
+  )
+    throw new Error('Error (stackEmpty): invalid parameter given');
   return stack.arr.length === 0;
 }
 
 function stackPush(stack, data) {
+  if (
+    stack === undefined ||
+    stack === null ||
+    !(typeof stack === 'object' && stack.arr.constructor === Array)
+  )
+    throw new Error('Error (stackPush): invalid parameter given (first)');
+  else if (data === undefined || data === null)
+    throw new Error('Error (stackPush): invalid parameter given (second)');
   let temp = [];
   temp = stack.arr;
   temp.length = stack.arr.length + 1;
@@ -15,12 +29,24 @@ function stackPush(stack, data) {
 }
 
 function stackPeek(stack) {
-  if (stack.arr.length === 0)
-    throw new Error('Error (stackPop): Stack is empty');
+  if (
+    stack === undefined ||
+    stack === null ||
+    !(typeof stack === 'object' && stack.arr.constructor === Array)
+  )
+    throw new Error('Error (stackPeek): invalid parameter given');
+  else if (stack.arr.length === 0)
+    throw new Error('Error (stackPeek): Stack is empty');
   return stack.arr[stack.arr.length - 1];
 }
 
 function stackPop(stack) {
+  if (
+    stack === undefined ||
+    stack === null ||
+    !(typeof stack === 'object' && stack.arr.constructor === Array)
+  )
+    throw new Error('Error (stackPop): invalid parameter given');
   if (stack.arr.length === 0)
     throw new Error('Error (stackPop): Stack is empty');
   let temp = [];
@@ -49,5 +75,7 @@ function stackPop(stack) {
 // stackPush(stackCheck, 'adsfadsf');
 
 // console.log(stackPeek(stackCheck));
-// if (stackEmpty(stackCheck)) console.log('stack empty');
+// if (stackEmpty('aaa')) console.log('stack empty');
 // else console.log('stack not empty');
+
+// console.log(stackPeek(ddd));

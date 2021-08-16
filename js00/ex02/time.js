840896e81f67afbee55626828c43a794e95acb25
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-16 15:42:58 +0900

    🩹 Change variable name

--- js00/ex02/time.js
+++ js00/ex02/time.js
@@ -1,13 +1,13 @@
-const month = { var: 7 };
+const month = { val: 7 };
 let day = 13;
 
 const timeWarp = (newMonth, newDay) => {
-  month.var = newMonth;
+  month.val = newMonth;
   day = newDay;
 };
 
-// console.log(`month: ${month.var} day: ${day}`);
+// console.log(`month: ${month.val} day: ${day}`);
 // timeWarp(5, 10);
-// console.log(`month: ${month.var} day: ${day}`);
+// console.log(`month: ${month.val} day: ${day}`);
 // timeWarp(11, 20);
-// console.log(`month: ${month.var} day: ${day}`);
+// console.log(`month: ${month.val} day: ${day}`);

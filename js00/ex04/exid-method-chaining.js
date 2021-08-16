6f012c341075d807caeb2ccdb06d69a4f22ff3d6
Chloekkk <skdusdl8804@gmail.com>
2021-08-16 23:21:02 +0900

    ex04 : complete

---
+++ js00/ex04/exid-method-chaining.js
@@ -0,0 +1,28 @@
+const exidMethodChaining = {
+    count: 40,
+    up() {
+        this.count++;
+        return this;
+    },
+    down() {
+        this.count--;
+        return this;
+    },
+    showCount() {
+        console.log(this.count);
+        return this;
+    }
+};
+
+exidMethodChaining
+    .up()
+    .down()
+    .up()
+    .up()
+    .down()
+    .up()
+    .down()
+    .up()
+    .up()
+    .down()
+    .showCount()

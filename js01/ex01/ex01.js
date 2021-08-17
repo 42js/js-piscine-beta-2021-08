82b5f139899087435761e10f577458adaee86df3
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-17 21:17:47 +0900

    ♻️ Refactor code. Use forEach and objects.

--- js01/ex01/ex01.js
+++ js01/ex01/ex01.js
@@ -1,20 +1,53 @@
-function decreaseValueAndColorRead(className, amount) {
-  const info = document.getElementsByClassName(className)[0];
+function displayBarsVertical(polarInfos, dirClassName) {
+  const minValue = 0;
+  const maxValue = 100;
+  const div = document.getElementsByClassName(dirClassName)[0];
 
-  info.setAttribute('value', parseInt(info.getAttribute('value'), 10) - amount);
-  info.style.setProperty('--c', 'red');
+  Object.keys(polarInfos).forEach((info) => {
+    const span = document.createElement('span');
+    span.append(polarInfos[info].barName);
+    const br = document.createElement('br');
+    const progress = document.createElement('progress');
+    progress.setAttribute('value', polarInfos[info].value);
+    progress.setAttribute('min', minValue);
+    progress.setAttribute('max', maxValue);
+    progress.setAttribute('class', info);
+
+    div.append(span, br, progress, br);
+  });
+  console.log(div);
 }
 
-function increaseValue(className, amount) {
-  const info = document.getElementsByClassName(className)[0];
+function displayBars() {
+  const leftPolarInfos = {
+    poo: { barName: '용변', value: 25 },
+    hungry: { barName: '허기', value: 50 },
+    energy: { barName: '에너지', value: 15 },
+  };
+  const rightPolarInfos = {
+    coding: { barName: '코딩력', value: 80 },
+    love: { barName: '덕력', value: 95 },
+    wach: { barName: '위생', value: 90 },
+  };
+  displayBarsVertical(leftPolarInfos, 'left');
+  displayBarsVertical(rightPolarInfos, 'right');
+}
 
-  info.setAttribute('value', parseInt(info.getAttribute('value'), 10) + amount);
+function decreaseValueAndColorRead(progress, amount) {
+  progress.setAttribute('value', parseInt(progress.getAttribute('value'), 10) - amount);
+  progress.style.setProperty('--c', 'red');
+}
+
+function increaseValue(progress, amount) {
+  progress.setAttribute('value', parseInt(progress.getAttribute('value'), 10) + amount);
 }
 
 window.onload = function process() {
-  decreaseValueAndColorRead('energy', 10);
-  const classNames = ['poo', 'hungry', 'energy', 'coding', 'love', 'wash'];
-  setTimeout(() => classNames.forEach((className) => {
-    increaseValue(className, 5);
+  displayBars();
+  
+  const progresses = document.querySelectorAll('progress');
+  decreaseValueAndColorRead(progresses[2], 10);
+  setTimeout(() => progresses.forEach((progress) => {
+    increaseValue(progress, 5);
   }), 5000);
 };

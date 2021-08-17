1ab86dc32c40adcf8b0808dea79d2afc6a6a4756
jikwon <jikwon@student.42seoul.kr>
2021-08-17 21:51:15 +0900

    update

--- js01/ex00/size.js
+++ js01/ex00/size.js
@@ -1,36 +1,20 @@
 function addTime()
 {
-	const p = document.createElement("p");
 	let today = new Date();
 	let hours = today.getHours();
 	let minutes = today.getMinutes();
 	let seconds = today.getSeconds();
-	let timetext = hours + ":" + minutes + ":" + seconds;
+	let timetext;
 	const timetag = document.getElementsByTagName("time");
-	const main = document.getElementsByTagName("main");
-	timetag[0].setAttribute("datetime", timetext);
+	timetag[0].setAttribute("datetime", today.toISOString());
 	if (hours >= 12)
 		timetext = "오후 " + (hours - 12);
 	else
 		timetext = "오전 ";
 	timetext += ":" + minutes + ":" + seconds;
-	const t = document.createTextNode(timetext);
-	p.appendChild(t);
-	main[0].insertBefore(p, timetag[0]);
+	timetag[0].innerHTML = timetext;
 }
 
-
-let scrollHeight = Math.max(
-	document.body.scrollHeight, document.documentElement.scrollHeight,
-	document.body.offsetHeight, document.documentElement.offsetHeight,
-	document.body.clientHeight, document.documentElement.clientHeight
-);
-
-let scrollWidth= Math.max(
-	document.body.scrollWidth, document.documentElement.scrollWidth,
-	document.body.offsetWidth, document.documentElement.offsetWidth,
-	document.body.clientWidth, document.documentElement.clientWidth
-);
 function addList()
 {
 	const ul = document.getElementsByTagName('ul');
@@ -43,7 +27,7 @@ function addList()
 	const text1 = "Screen: " + screen.width + " * " + screen.height;
 	const text2 = "Window Outer: " + window.outerWidth + " * " + window.outerHeight;
 	const text3 = "Window Inner: " + window.innerWidth + " * " + window.innerHeight;
-	const text4 = "Document: " + scrollWidth + " * " + scrollHeight
+	const text4 = "Document: " + document.body.scrollWidth + " * " + document.body.scrollHeight
 
 	const textNode1 = document.createTextNode(text1);
 	const textNode2 = document.createTextNode(text2);

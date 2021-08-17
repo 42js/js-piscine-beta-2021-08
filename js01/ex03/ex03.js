633c91833aad417f449f4e20b442b02e91086d98
kunlee <leekh716@naver.com>
2021-08-17 17:55:13 +0900

    js01 edit

--- js01/ex03/ex03.js
+++ js01/ex03/ex03.js
@@ -1,14 +1,22 @@
 const menus = document.getElementsByTagName('li');
 Array.from(menus).forEach(menu => {
-	menu.addEventListener('click', func);
+	menu.addEventListener('click', onClick);
+	menu.addEventListener('mouseover', onMouseOver);
+	menu.addEventListener('mouseout', onMouseOut);
 });
 
-function func(e) {
-	Array.from(menus).forEach(menu => {
-		menu.style.backgroundColor = 'cornflowerblue'
-	});
+function onClick(e) {
+	const select = e.target.parentElement.parentElement;
+	const selectSection = select.querySelector('h2').innerText;
+	alert(`오늘의 메뉴는 ${selectSection} 중 ${e.target.innerText}!`)
+}
+
+function onMouseOver(e) {
+	e.stopPropagation();
 	e.target.style.backgroundColor = 'red'
-	setTimeout(function(){
-		alert(`오늘의 메뉴는 ${e.target.innerText}!`)
-	}, 100);
+}
+
+function onMouseOut(e) {
+	e.stopPropagation();
+	e.target.style.backgroundColor = 'cornflowerblue'
 }

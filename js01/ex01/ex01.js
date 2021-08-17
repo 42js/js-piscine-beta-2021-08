bf5a60d47de946444952aff102ca42df60158cd0
Sungyong Cho <to.sungyongcho@gmail.com>
2021-08-17 20:00:46 +0900

    ex01 done

---
+++ js01/ex01/ex01.js
@@ -0,0 +1,28 @@
+const progress = document.querySelectorAll('progress');
+
+function setEnergyProgressToLow() {
+  const energyProgress = document.querySelector('.energy');
+  // console.log(energyProgress.getAttribute('value'));
+  let val = Number(energyProgress.getAttribute('value'));
+  val -= 10;
+
+  energyProgress.setAttribute('value', val);
+  energyProgress.style.setProperty('--c', 'red');
+}
+
+function addProgressAllTen() {
+  for (let i = 0; i < progress.length; i += 1) {
+    let val = Number(progress[i].getAttribute('value'));
+    val += 5;
+    // console.log(val);
+    progress[i].setAttribute('value', `${val}`);
+  }
+}
+
+window.onload = function exZeroOne() {
+  setEnergyProgressToLow();
+
+  setTimeout(() => {
+    addProgressAllTen();
+  }, 3000);
+};

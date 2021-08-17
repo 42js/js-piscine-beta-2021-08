b67d1db7099f8d821e67b71422371786c2cc36c4
from97 <skm1104@daum.net>
2021-08-17 13:22:24 +0900

    js01/ex02 finished

--- js01/ex02/calc.js
+++ js01/ex02/calc.js
@@ -30,9 +30,48 @@ const form_item = {
     grade: form.grade
 };
 
+const receipt = {
+    name: document.querySelector('.name'),
+    price: document.querySelector('.price'),
+    salePrice: document.querySelector('.sale_price')
+}
 
+const checkIsMember = (e) => {
+    if (e.target.checked)
+        form_item.grade.removeAttribute("disabled");
+    else
+    {
+        form_item.grade.setAttribute("disabled", "true");
+        form_item.grade.value = ''
+    }
+    handleOptionSelected();
+}
 
+const handleNameChange = (e) => {
+    receipt.name.innerHTML = '상품명: ' + e.target.value;
+}
 
+const handleOptionSelected = () => {
+    let salePrice = Number(form_item.price.value);
+    if (form_item.grade.value === 'lv1')
+        salePrice -= salePrice * 5 / 100;
+    else if (form_item.grade.value === 'lv2')
+        salePrice -= salePrice * 10 / 100;
+    else if (form_item.grade.value === 'lv3')
+        salePrice -= salePrice * 15 / 100;
+    salePrice = salePrice.toLocaleString();
+    receipt.salePrice.innerHTML = `할인가: ${salePrice}`;
+}
+
+const handlePriceChange = (e) => {
+    receipt.price.innerHTML = '원가: ' + Number(e.target.value).toLocaleString();
+    handleOptionSelected();
+}
+
+form_item.isMember.addEventListener('change', checkIsMember);
+form_item.name.addEventListener('change', handleNameChange);
+form_item.price.addEventListener('change', handlePriceChange);
+form_item.grade.addEventListener('change', handleOptionSelected);
 
 
 

04f7a31596ed2fc5981bfe621d10a24439bf0b3e
evelon <ezemango@gmail.com>
2021-08-17 13:45:39 +0900

    ex03 maybe complete

---
+++ js01/ex03/ex03.js
@@ -0,0 +1,18 @@
+
+const changeColorHotpink = (event) => {
+  event.target.style.backgroundColor = 'hotpink';
+}
+
+const changeColorCornflowerblue = (event) => {
+  event.target.style.backgroundColor = 'cornflowerblue';
+}
+
+const li = document.getElementsByTagName('li');
+
+console.log(li);
+
+for (element of li) {
+  element.addEventListener('mouseover', changeColorHotpink);
+  element.addEventListener('mouseout', changeColorCornflowerblue);
+  element.addEventListener('click', () => {alert("선택 완료")});
+}

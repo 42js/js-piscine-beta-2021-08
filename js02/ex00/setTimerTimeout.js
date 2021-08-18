549771f325b27a9f5afcefe227833eff4a95ca71
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-18 23:26:55 +0900

    🩹 약간의 수정

--- js02/ex00/setTimerTimeout.js
+++ js02/ex00/setTimerTimeout.js
@@ -2,7 +2,9 @@ export default function setTimer(callback, interval) {
   let count = 0;
   const timeId = setTimeout(function go() {
     if (!callback(count)) clearTimeout(timeId);
-    else setTimeout(go, interval);
-    count += 1;
+    else {
+      count += 1;
+      setTimeout(go, interval);
+    }
   }, interval);
 }

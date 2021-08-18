cc9c79c9f55d8d4b846e0fec0f6c6e3098b81d72
Sungyong Cho <to.sungyongcho@gmail.com>
2021-08-18 23:13:35 +0900

    ex02 done

--- js02/ex02/recipe_promise.js
+++ js02/ex02/recipe_promise.js
@@ -1,84 +1,86 @@
-function randomFail() {
-  if (Math.random() < 0.2) throw '제작 실패..!(월급이 삭감되었다 ㅜㅜ)';
-}
-
-class ReceipePromise {
-  makeBatch() {
-    return new Promise((resolve, reject) => {
-      console.log('반죽 시작');
-      setTimeout(() => {
-        try {
-          randomFail();
-          const result = '반죽 성공';
-          resolve(result);
-        } catch (error) {
-          console.error(error);
-          reject(this.error);
-        }
-      }, 100);
+function frying() {
+  console.log('[2차 발효] 시작');
+  const promiseSecondPhase = new Promise((resolve, reject) => {
+    if (Math.random() <= 0.2) setTimeout(resolve, 5000);
+    else setTimeout(reject, 5000);
+  });
+  promiseSecondPhase
+    .then(() => {
+      console.log('[튀기기] 성공');
+      console.log('끝');
+    })
+    .catch(() => {
+      console.log('[튀기기] 제작 실패..!(월급이 삭감되었다 ㅜㅜ)');
+      frying();
     });
-  }
-
-  // firstFermentation(onSuccess, onError) {
-  //   console.log('1차 발효 시작');
-  //   setTimeout(() => {
-  //     try {
-  //       randomFail();
-  //       const result = '1차 발효 성공';
-  //       onSuccess(result);
-  //     } catch (error) {
-  //       console.error(error);
-  //       onError(this.firstFermentation(onSuccess, onError));
-  //     }
-  //   }, 5000);
-  // }
-
-  // shapingBread(onSuccess, onError) {
-  //   console.log('성형 하기 시작');
-  //   setTimeout(() => {
-  //     try {
-  //       randomFail();
-  //       const result = '성형 하기 성공';
-  //       onSuccess(result);
-  //     } catch (error) {
-  //       console.error(error);
-  //       onError(this.shapingBread(onSuccess, onError));
-  //     }
-  //   }, 4200);
-  // }
-
-  // secondFermentation(onSuccess, onError) {
-  //   console.log('2차 발효 시작');
-  //   setTimeout(() => {
-  //     try {
-  //       randomFail();
-  //       const result = '2차 발효 성공';
-  //       onSuccess(result);
-  //     } catch (error) {
-  //       console.error(error);
-  //       onError(this.secondFermentation(onSuccess, onError));
-  //     }
-  //   }, 2000);
-  // }
-
-  // frying(onSuccess, onError) {
-  //   console.log('튀기기 시작');
-  //   setTimeout(() => {
-  //     try {
-  //       randomFail();
-  //       const result = '튀기기 성공';
-  //       onSuccess(result);
-  //     } catch (error) {
-  //       console.error(error);
-  //       onError(this.frying(onSuccess, onError));
-  //     }
-  //   }, 5000);
-  // }
 }
 
-const recipePromise = new ReceipePromise();
+function secondFermentation() {
+  console.log('[2차 발효] 시작');
+  const promiseSecondPhase = new Promise((resolve, reject) => {
+    if (Math.random() <= 0.2) setTimeout(resolve, 2000);
+    else setTimeout(reject, 2000);
+  });
+  promiseSecondPhase
+    .then(() => {
+      console.log('[2차 발효] 성공');
+      frying();
+    })
+    .catch(() => {
+      console.log('[2차 발효] 제작 실패..!(월급이 삭감되었다 ㅜㅜ)');
+      secondFermentation();
+    });
+}
 
-recipePromise
-  .makeBatch()
-  .catch(() => {})
-  .then((result) => console.log(result));
+function shapingBread() {
+  console.log('[성형 하기] 시작');
+  const promiseSecondPhase = new Promise((resolve, reject) => {
+    if (Math.random() <= 0.2) setTimeout(resolve, 4200);
+    else setTimeout(reject, 4200);
+  });
+  promiseSecondPhase
+    .then(() => {
+      console.log('[2차 발효] 성공');
+      secondFermentation();
+    })
+    .catch(() => {
+      console.log('[성형 하기] 제작 실패..!(월급이 삭감되었다 ㅜㅜ)');
+      shapingBread();
+    });
+}
+
+function firstFermentation() {
+  console.log('[1차 발효] 시작');
+  const promiseSecondPhase = new Promise((resolve, reject) => {
+    if (Math.random() <= 0.2) setTimeout(resolve, 5000);
+    else setTimeout(reject, 5000);
+  });
+  promiseSecondPhase
+    .then(() => {
+      console.log('[1차 발효] 성공');
+      shapingBread();
+    })
+    .catch(() => {
+      console.log('[1차 발효] 제작 실패..!(월급이 삭감되었다 ㅜㅜ)');
+      firstFermentation();
+    });
+}
+
+function makeBatch() {
+  console.log('[반죽] 시작');
+  const promiseFirstPhase = new Promise((resolve, reject) => {
+    if (Math.random() <= 0.2) setTimeout(resolve, 3000);
+    else setTimeout(reject, 3000);
+  });
+  promiseFirstPhase
+    .then(() => {
+      console.log('[반죽] 성공');
+      firstFermentation();
+    })
+    .catch(() => {
+      console.log('[반죽] 제작 실패..!(월급이 삭감되었다 ㅜㅜ)');
+      makeBatch();
+    });
+}
+
+makeBatch();

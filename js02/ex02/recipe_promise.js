fde9927d8d604bf3986d99671bb40a36228064be
evelon <ezemango@gmail.com>
2021-08-18 15:31:26 +0900

    ex02 changed

--- js02/ex02/recipe_promise.js
+++ js02/ex02/recipe_promise.js
@@ -10,58 +10,45 @@ const sec3 = 3;
 const sec4 = 4;
 const sec5 = 5;
 
+const oneStep = (step, second, resolve, reject) => {
+  console.log(step);
+  setTimeout(() => {
+    if (Math.random() < 0.2) reject("제작 실패..!(월급이 삭감되었다 ㅜㅜ)");
+    resolve();
+  }, second * 1000);
+}
 
 const fifthStep = () => {
   let promise = new Promise((resolve, reject) => {
-    console.log(step5);
-    setTimeout(() => {
-      if (Math.random() < 0.2) reject("제작 실패..!(월급이 삭감되었다 ㅜㅜ)");
-      resolve();
-    }, sec5 * 1000);
+    oneStep(step5, sec5, resolve, reject);
   });
   promise.then(() => { console.log('꽈배기 하나 완성!'); }).catch((e) => { console.log(e); fifthStep(); });
 }
 
 const fourthStep = () => {
   let promise = new Promise((resolve, reject) => {
-    console.log(step4);
-    setTimeout(() => {
-      if (Math.random() < 0.2) reject("제작 실패..!(월급이 삭감되었다 ㅜㅜ)");
-      resolve();
-    }, sec4 * 1000);
+    oneStep(step4, sec4, resolve, reject);
   });
   promise.then(fifthStep).catch((e) => { console.log(e); fourthStep(); });
 }
 
 const thirdStep = () => {
   let promise = new Promise((resolve, reject) => {
-    console.log(step3);
-    setTimeout(() => {
-      if (Math.random() < 0.2) reject("제작 실패..!(월급이 삭감되었다 ㅜㅜ)");
-      resolve();
-    }, sec3 * 1000);
+    oneStep(step3, sec3, resolve, reject);
   });
   promise.then(fourthStep).catch((e) => { console.log(e); thirdStep(); });
 }
 
 const secondStep = () => {
   let promise = new Promise((resolve, reject) => {
-    console.log(step2);
-    setTimeout(() => {
-      if (Math.random() < 0.2) reject("제작 실패..!(월급이 삭감되었다 ㅜㅜ)");
-      resolve();
-    }, sec2 * 1000);
+    oneStep(step2, sec2, resolve, reject);
   });
   promise.then(thirdStep).catch((e) => { console.log(e); secondStep(); });
 }
 
 const firstStep = () => {
   let promise = new Promise((resolve, reject) => {
-    console.log(step1);
-    setTimeout(() => {
-      if (Math.random() < 0.2) reject("제작 실패..!(월급이 삭감되었다 ㅜㅜ)");
-      resolve();
-    }, sec1 * 1000);
+    oneStep(step1, sec1, resolve, reject);
   });
   promise.then(secondStep).catch((e) => { console.log(e); firstStep() });
 }

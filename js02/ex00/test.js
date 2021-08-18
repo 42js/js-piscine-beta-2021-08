2288df501011c7481ca76646163c2f79d40e67b9
Sungyong Cho <to.sungyongcho@gmail.com>
2021-08-18 23:28:20 +0900

    remove ex00/test.js

--- js02/ex00/test.js
+++
@@ -1,8 +0,0 @@
-import setTimer from './setTimerTimeout.js';
-
-function printNum(count) {
-  console.log(count);
-  return count < 3;
-}
-
-setTimer(printNum, 100); // 0 1 2 3 <= 각각 100ms의 간격으로 출력됨.

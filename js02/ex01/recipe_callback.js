c72a7b9e0a7c68a6e4a69dbb37d62f9484718009
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-18 15:58:56 +0900

    🗑수정해야하는 코드. throw에 문제 있음.

--- js02/ex01/recipe_callback.js
+++ js02/ex01/recipe_callback.js
@@ -1,25 +1,16 @@
 function randomFail() {
-  if (Math.random() < 0.8) throw String('제작 실패..!(월급이 삭감되었다 ㅜㅜ)');
-}
-
-function setTimer(callback, interval) {
-  const timeId = setTimeout(function start() {
-    try {
-      randomFail();
-      callback();
-      clearTimeout(timeId);
-    } catch (e) {
-      setTimeout(start, interval);
-      console.log(e);
-    }
-  }, interval);
+  if (Math.random() < 0.2) throw String('제작 실패..!(월급이 삭감되었다 ㅜㅜ)');
 }
 
 function showSuccess(stepTodo) {
   console.log(`${stepTodo} 성공!`);
 }
 
-function makeTwistedDoughnut() {
+function showFail(stepTodo) {
+  console.log(`${stepTodo} 실패...`);
+}
+
+function makeTwistedDoughnut(callback) {
   const workSteps = {
     1: { todo: '반죽만들기', time: 3000 },
     2: { todo: '1차 발효', time: 5000 },
@@ -27,9 +18,24 @@ function makeTwistedDoughnut() {
     4: { todo: '2차 발효', time: 2000 },
     5: { todo: '튀기기', time: 5000 },
   };
-  Object.keys(workSteps).forEach((step) => {
-    setTimer(showSuccess(step.todo), step.time);
-  });
+  let step = 1;
+  const timeId = setTimeout(function go() {
+    try {
+      callback();
+      clearTimeout(timeId);
+      showSuccess(workSteps[step].todo);
+      step += 1;
+    } catch (e) {
+      showFail(workSteps[step].todo);
+      console.log(e);
+    }
+    if (step > 5) throw String('꽈베기 완성@^.^@');
+    setTimeout(go, workSteps[step].time);
+  }, workSteps[step].time);
 }
 
-makeTwistedDoughnut();
+try {
+  makeTwistedDoughnut(randomFail);
+} catch (e) {
+  console.log(e);
+}

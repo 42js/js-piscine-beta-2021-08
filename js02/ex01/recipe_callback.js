b946a1cd622a543ec929f7379855d4a02c70c0c7
from97 <skm1104@daum.net>
2021-08-18 21:41:34 +0900

    js02/ex01 finished

--- js02/ex01/recipe_callback.js
+++ js02/ex01/recipe_callback.js
@@ -3,60 +3,29 @@ function randomFail() {
     if (Math.random() < 0.2) throw "제작 실패..!(월급이 삭감되었다 ㅜㅜ)";
 }
 
-/*
-꽈배기 만들기
-
-1. 반죽 만들기 - 3초 kneading
-2. 1차 발효 - 5초 firstFermentation
-3. 성형 하기 - 4.2초 forming
-4. 2차 발효 - 2초 secondFermentation
-5. 튀기기 - 5초 frying
-*/
-
-const kneading = () => {
+const doStep = (step) => {
     try {
         randomFail();
-        console.log('kneading');
+        console.log(step);
+        return true;
     } catch (error) {
-        console.log('kneading: '+ error);
+        console.log(`${step}: ${error}`);
+        return false;
     }
-};
+}
 
-const firstFermentation = () => {
-    try { 
-        randomFail();
-        console.log('firstFermentation');
-    } catch (error) {
-        console.log('firstFermentation: '+ error);
-    }
-};
-
-const forming = () => {
-    try { 
-        randomFail();
-        console.log('forming');
-    } catch (error) {
-        console.log('forming: '+ error);
-    }
-};
-
-const secondFermentation = () => {
-    try { 
-        randomFail();
-        console.log('secondFermentation');
-    } catch (error) {
-        console.log('secondFermentation: '+ error);
-    }
-};
-
-const frying = () => {
-    try { 
-        randomFail();
-        console.log('frying');
-    } catch (error) {
-        console.log('frying: '+ error);
-    }
-};
-
-setTimeout(kneading, 300);
-
+setTimeout(() => {
+    while (!doStep('kneading'));
+    setTimeout(() => {
+        while (!doStep('firstFermentation'));
+        setTimeout(() => {
+            while (!doStep('forming'));
+            setTimeout(() => {
+                while (!doStep('secondFermentation'));
+                setTimeout(() => {
+                    while (!doStep('frying'));
+                }, 500);
+            }, 200);
+        }, 420);
+    }, 500);
+}, 300);

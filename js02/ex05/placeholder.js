6d17580bd50663e6beab850eb7bee366865a59f6
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2021-08-18 23:58:22 +0900

    go

--- js02/ex05/placeholder.js
+++ js02/ex05/placeholder.js
@@ -1,20 +1,30 @@
+import axios from "axios";
+
 let randNum = []
 
 for (var i = 0; i < 5; i++) randNum[i] = parseInt(Math.random() * 100);
 
 console.log(randNum)
 
-// function pull(postId) {
-//     return new Promise((resolve, reject) => {
-//     }).then((go) => {
-//         console.log(go);
-//     })
-// }
+async function pull(postId) {
+    try {
+        const post = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+        const comment = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`);
+        console.log(`제목 : ${post.data.title}\n`);
+        console.log(post.data.body);
+        comment.data.map((com) => {
+			console.log('---');
+			console.log(`댓글 작성자: ${com.name}`);
+			console.log(`이메일: ${com.email}`);
+			console.log(com.body);
+		});
+        console.log("");
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
 
-
-
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
-
-pull(randNum[0])
+randNum.map(postId => {
+    pull(postId);
+})

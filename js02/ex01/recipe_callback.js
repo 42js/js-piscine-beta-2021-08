73861280c9e8c57bcb7d73e98f6a3bfcdc6384cf
evelon <ezemango@gmail.com>
2021-08-18 18:27:15 +0900

    js02 complete

--- js02/ex01/recipe_callback.js
+++ js02/ex01/recipe_callback.js
@@ -1,74 +1,38 @@
+const step1 = '1. 반죽 만들기 - 3초';
+const step2 = '2. 1차 발효 - 5초';
+const step3 = '3. 성형 하기 - 4.2초';
+const step4 = '4. 2차 발효 - 2초';
+const step5 = '5. 튀기기 - 5초';
+const complete = '꽈배기 하나 완성!';
+const steps = [step1, step2, step3, step4, step5, complete];
+
+const sec1 = 3;
+const sec2 = 5;
+const sec3 = 4.2;
+const sec4 = 2;
+const sec5 = 5;
+const seconds = [sec1, sec2, sec3, sec4, sec5]
+
 const randomFail = () => {
   if (Math.random() < 0.2) throw "제작 실패..!(월급이 삭감되었다 ㅜㅜ)";
 }
 
-const fifthStep = () => {
-  console.log('5. 튀기기 - 5초');
+const oneStep = (i) => {
+  console.log(steps[i]);
+  if (i === 5) return;
   setTimeout(() => {
     try {
       randomFail();
-      console.log('꽈배기 하나 완성!');
+      oneStep(i + 1);
     } catch (e) {
       console.log(e);
-      fifthStep();
+      oneStep(i);
     }
-  }, 5 * 1000);
-}
-
-const fourthStep = () => {
-  console.log('4. 2차 발효 - 2초');
-  setTimeout(() => {
-    try {
-      randomFail();
-      fifthStep();
-    } catch (e) {
-      console.log(e);
-      fourthStep();
-    }
-  }, 2 * 1000);
-}
-
-const thirdStep = () => {
-  console.log('3. 성형 하기 - 4.2초');
-  setTimeout(() => {
-    try {
-      randomFail();
-      fourthStep();
-    } catch (e) {
-      console.log(e);
-      thirdStep();
-    }
-  }, 4.2 * 1000);
-}
-
-const secondStep = () => {
-  console.log('2. 1차 발효 - 5초');
-  setTimeout(() => {
-    try {
-      randomFail();
-      thirdStep();
-    } catch (e) {
-      console.log(e);
-      secondStep();
-    }
-  }, 5 * 1000);
-}
-
-const firstStep = () => {
-  console.log('1. 반죽 만들기 - 3초');
-  setTimeout(() => {
-    try {
-      randomFail();
-      secondStep();
-    } catch (e) {
-      console.log(e);
-      firstStep(step, second);
-    }
-  }, 3 * 1000);
+  }, seconds[i] * 1000);
 }
 
 const fryDonut = function() {
-  firstStep();
+  oneStep(0);
 }
 
 fryDonut();
--- js02/ex02/recipe_promise.js
+++ js02/ex02/recipe_promise.js
@@ -3,56 +3,38 @@ const step2 = '2. 1차 발효 - 5초';
 const step3 = '3. 성형 하기 - 4.2초';
 const step4 = '4. 2차 발효 - 2초';
 const step5 = '5. 튀기기 - 5초';
+const steps = [step1, step2, step3, step4, step5];
+const complete = '꽈배기 하나 완성!';
 
 const sec1 = 3;
 const sec2 = 5;
-const sec3 = 3;
-const sec4 = 4;
+const sec3 = 4.2;
+const sec4 = 2;
 const sec5 = 5;
+const seconds = [sec1, sec2, sec3, sec4, sec5]
 
-const oneStep = (step, second, resolve, reject) => {
-  console.log(step);
-  setTimeout(() => {
-    if (Math.random() < 0.2) reject("제작 실패..!(월급이 삭감되었다 ㅜㅜ)");
-    resolve();
-  }, second * 1000);
-}
-
-const fifthStep = () => {
+const oneStep = (i) => {
   let promise = new Promise((resolve, reject) => {
-    oneStep(step5, sec5, resolve, reject);
-  });
-  promise.then(() => { console.log('꽈배기 하나 완성!'); }).catch((e) => { console.log(e); fifthStep(); });
+    console.log(steps[i]);
+    setTimeout(() => {
+      if (Math.random() < 0.2) reject("제작 실패..!(월급이 삭감되었다 ㅜㅜ)");
+      resolve();
+    }, seconds[i] * 1000);
+  })
+  promise
+    .then(() => {
+      if (i === 4) {
+        console.log(complete);
+      } else {
+        oneStep(i + 1);
+      }
+    })
+    .catch((e) => {
+      console.log(e);
+      oneStep(i);
+    })
 }
 
-const fourthStep = () => {
-  let promise = new Promise((resolve, reject) => {
-    oneStep(step4, sec4, resolve, reject);
-  });
-  promise.then(fifthStep).catch((e) => { console.log(e); fourthStep(); });
-}
-
-const thirdStep = () => {
-  let promise = new Promise((resolve, reject) => {
-    oneStep(step3, sec3, resolve, reject);
-  });
-  promise.then(fourthStep).catch((e) => { console.log(e); thirdStep(); });
-}
-
-const secondStep = () => {
-  let promise = new Promise((resolve, reject) => {
-    oneStep(step2, sec2, resolve, reject);
-  });
-  promise.then(thirdStep).catch((e) => { console.log(e); secondStep(); });
-}
-
-const firstStep = () => {
-  let promise = new Promise((resolve, reject) => {
-    oneStep(step1, sec1, resolve, reject);
-  });
-  promise.then(secondStep).catch((e) => { console.log(e); firstStep() });
-}
-
-const fryDonut = () => { firstStep(); };
+const fryDonut = () => { oneStep(0); };
 
 fryDonut();

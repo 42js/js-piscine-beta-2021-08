9afe7d3c82c551c73aaa140c613602c4672ca872
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-19 22:50:29 +0900

    Css stype변경, subject에 불필요한 작동 제거

--- fe00/ex02/todo-list/src/components/Item.js
+++ fe00/ex02/todo-list/src/components/Item.js
@@ -3,28 +3,15 @@ import './Item.css';
 
 // text: todo 내용
 // checked: 체크박스 상태
-// id: todo의 고유 아이디
-// onToggle: 체크박스를 키고 끄는 함수
-// onRemove: 아이템을 삭제시키는 함수
-
 class Item extends Component {
   render() {
-    const { text, checked, id, onToggle, onRemove } = this.props;
+    const { text, checked } = this.props;
     return (
-      <div className="todo-item" onClick={() => onToggle(id)}>
-        <div
-          className="remove"
-          onClicnk={(e) => {
-            e.stopPropagation();
-            onRemove(id);
-          }}
-        >
-          &times;
-        </div>
+      <div className="todo-item">
         <div className={`todo-text ${checked && 'checked'}`}>
           <div>{text}</div>
         </div>
-        {checked && <div className="check-mark">✓</div>}
+        <div className="remove">&times;</div>
       </div>
     );
   }

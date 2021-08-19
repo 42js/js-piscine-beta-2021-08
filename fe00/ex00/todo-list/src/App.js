53fb03b48abad56bdb2cb4b74692b171d9545fd1
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-19 22:23:48 +0900

    App 함수 사용하도록 수정

--- fe00/ex00/todo-list/src/App.js
+++ fe00/ex00/todo-list/src/App.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
-import TodoListTemplate from './components/TodoListTemplate';
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <TodoListTemplate
-        form="입력할 부분"
-        children="할일 리스트 부분"
-      ></TodoListTemplate>
-    );
-  }
+function App() {
+  return (
+    <main className="todo-list-template">
+      <div className="title">ToDoList</div>
+      <section className="form-wrapper">입력할 부분</section>
+      <section className="todos-wrapper">할일 리스트 부분</section>
+    </main>
+  );
 }
 
 export default App;

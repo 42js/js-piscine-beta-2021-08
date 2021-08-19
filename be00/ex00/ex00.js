256938ec30b10ae28634660d72c76d225658938e
강명구 <mg5566@hanmail.net>
2021-08-19 15:01:53 +0900

    complete ex00 and test

--- be00/ex00/ex00.js
+++ be00/ex00/ex00.js
@@ -2,11 +2,18 @@ console.log("Fuck you");
 
 console.log("rewrite");
 
-var http = require('http');
+const http = require('http');
 
-http.createServer((req, res) => {
-	res.write('hello');
-	res.end();
-}).listen(8080);
+const server = http.createServer((req, res) => {
+	if (req.method === 'GET')
+		console.log(`GET 요청입니다.`);
+	res.statusCode = 200;
+	res.setHeader("Content-Language", "en");
+	res.setHeader("Content-Type", "text/plain");
+	// 한글이 안되네... curl 로 하면 잘 보임
+	res.end("안녕, Fucking Node.js");
+});
 
-console.log("fuck");
+server.listen(4242, () => {
+	console.log(`Server is running at http://localhost:4242`);
+})

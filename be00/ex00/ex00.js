c86ac43fd0df29089e4f16c4ca42d8514ba549db
Inho Ha <inhoha@Inhoui-MacBookPro.local>
2021-08-19 23:38:25 +0900

    ex02 bad

--- be00/ex00/ex00.js
+++ be00/ex00/ex00.js
@@ -5,7 +5,7 @@ const port = 4242;
 
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
-	res.setHeader("Content-Type", "text/html; charset=utf-8");
+	res.setHeader("Content-Type", "text/plain; charset=utf-8");
 	res.end("안녕, Node.js!");
 })
 
--- be00/ex02/ex02.js
+++ be00/ex02/ex02.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const { stringify } = require('querystring');
 
 const hostname = 'localhost';
 const port = 4242;
@@ -11,22 +12,45 @@ function send500Message(err, res) {
 	console.log(err);
 }
 
-function onRequest(req, res) {
-	if (req.method == 'GET' && req.url == '/') {
-		fs.readFile('index.html', (err, data) => {
-			try {
-				if (err) throw err;
-				else {
-					res.statusCode = 200;
-					res.setHeader("Content-Type", "text/html");
-					res.end(data);
-				}
+function takeFileData(req, res) {
+	fs.readFile('data.json', (err, data) => {
+		try {
+			if (err) throw err;
+			res.statusCode = 200;
+			const fileData = JSON.parse(data);
+			if (JSON.stringify(fileData) === "{}") {
+				res.setHeader("Content-Type", "text/plain");
+				res.end("empty")
 			}
-			catch (err) {
+			else {
+				const jsonData = {};
+				jsonData.message = "Content retrieved successfully.";
+				jsonData.data = fileData;
+				res.setHeader("Content-Type", "application/json");
+				res.end(JSON.stringify(jsonData));
+			}
+		}
+		catch (err) {
+			if (err.code == 'ENOENT') {
+				fs.writeFile("data.json", "{}", (err)=>{
+					send500Message(err, res);
+				});
+				res.statusCode = 500;
+				res.setHeader("Content-Type", "text/plain");
+				res.end(String(err));
+			}
+			else{
 				send500Message(err, res);
 			}
-		})
+		}
+	})
+}
+
+function onRequest(req, res) {
+	if (req.method == 'GET' && req.url == '/file') {
+		takeFileData(req, res);
 	}
+
 }
 
 const server = http.createServer(onRequest);

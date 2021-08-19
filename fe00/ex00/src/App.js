1bef990c744bee8928238328eea499d2e62930d9
Yenowme <76904042+Yenowme@users.noreply.github.com>
2021-08-19 14:06:37 +0900

    ex00: done

---
+++ fe00/ex00/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>ToDoList</h1>
+            <input value="입력할 부분"></input>
+            <section>할일 리스트 부분</section>
+        </div>
+    );
+}
+
+export default App;

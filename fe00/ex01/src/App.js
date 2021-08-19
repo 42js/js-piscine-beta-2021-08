df9b0ea0b3bf75a5359f1b8adbdfaf953717f425
from97 <skm1104@daum.net>
2021-08-19 14:37:16 +0900

    fe00/ex01 init - copied from ex00

---
+++ fe00/ex01/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>To Do List</h1>
+      </header>
+      <main>
+        <section className="inputSection">입력할 부분</section>
+        <section className="listSection">할일 리스트 부분</section>
+      </main>
+    </div>
+  );
+}
+
+export default App;

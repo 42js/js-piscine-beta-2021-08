af3c7b68121b3093a3ef88680ca1dc14ed019f6c
Yenowme <76904042+Yenowme@users.noreply.github.com>
2021-08-19 14:27:06 +0900

    ex01: done

---
+++ fe00/ex01/src/ToDoListTemplate.js
@@ -0,0 +1,13 @@
+import "./ToDoListTemplate.css";
+
+function ToDoList() {
+    return (
+        <div className="Todo">
+            <h1>ToDoList</h1>
+            <input value="입력할 부분"></input>
+            <section>할일 리스트 부분</section>
+        </div>
+    );
+}
+
+export default ToDoList;

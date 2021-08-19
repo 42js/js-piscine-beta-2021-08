dafa4d5e33f98840094aa8c30539b86a7184424b
Sungyong Cho <to.sungyongcho@gmail.com>
2021-08-19 18:18:33 +0900

    ex00 done

--- fe00/ex00/src/App.js
+++ fe00/ex00/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const TodoListTemplate = ({form, children}) => {
+
+  form = "입력할 부분";
+
+  return (
+    <main className="todo-list-template">
+      <div className="title">
+        TodoList
+      </div>
+      <section className="form-wrapper">
+        {form}
+      </section>
+      <section className="todos-wrapper">
+        할일 리스트 부분
+        { children }
+      </section>
+    </main>
+  );
+};
+
+export default TodoListTemplate;
--- fe00/ex00/src/index.js
+++ fe00/ex00/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TodoListTemplate from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoListTemplate />
   </React.StrictMode>,
   document.getElementById('root')
 );

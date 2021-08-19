67a09a9a102f25d055e21afd6b00a424687aa99e
from97 <skm1104@daum.net>
2021-08-19 15:00:51 +0900

    fe00/ex02 init - copied from ex01

---
+++ fe00/ex02/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ToDoListTemplate from "./ToDoListTemplate";
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoListTemplate />
+    </div>
+  );
+}
+
+export default App;
---
+++ fe00/ex02/src/ToDoListTemplate.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ToDoListTemplate.css";
+
+export default function ToDoListTemplate() {
+  return (
+    <div>
+      <header>
+        <h1>To Do List</h1>
+      </header>
+      <main>
+        <section className="inputSection">입력할 부분</section>
+        <section className="listSection">할일 리스트 부분</section>
+      </main>
+    </div>
+  );
+}

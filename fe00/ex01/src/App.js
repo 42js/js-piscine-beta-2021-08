d727c474bf0f070cf964f7c81b2d632cdea34abf
hyuntae <believe4279@gmail.com>
2021-08-19 18:47:40 +0900

    ex01 refactoring

--- fe00/ex01/src/App.js
+++ fe00/ex01/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import todolist from "./ToDoListTemplate";
+import ToDoList from "./ToDoListTemplate";
 import './ToDoListTemplate.css';
 
 function App() {
+  const 입력 = <ToDoList className="입력" name="입력할 부분"/>
+  const 출력 = <ToDoList className="출력" name="할일 리스트 부분"/>
   return (
     <div className="App">
       <h1 className="title">ToDoList</h1>
-      {todolist()}
+      {입력}
+      {출력}
     </div>
   );
 }
--- fe00/ex01/src/ToDoListTemplate.js
+++ fe00/ex01/src/ToDoListTemplate.js
@@ -1,8 +1,3 @@
-export default function ToDoList(){
-    return (
-        <div>
-            <section className="할일입력">입력할 부분</section>
-            <section className="할일출력">할일리스트 부분</section>     
-        </div>
-        )
-}
+export default function ToDoList(props){
+    return <section className={props.className}>{props.name}</section>
+  }

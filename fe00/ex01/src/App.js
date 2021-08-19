ea7c5332c083485ece13b52fb369f7b90631c1af
from97 <skm1104@daum.net>
2021-08-19 14:50:30 +0900

    ToDoListTemplate 컴포넌트 Import

--- fe00/ex01/src/App.js
+++ fe00/ex01/src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
+import "./ToDoListTemplate";
+import ToDoListTemplate from "./ToDoListTemplate";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>To Do List</h1>
-      </header>
-      <main>
-        <section className="inputSection">입력할 부분</section>
-        <section className="listSection">할일 리스트 부분</section>
-      </main>
+      <ToDoListTemplate />
     </div>
   );
 }

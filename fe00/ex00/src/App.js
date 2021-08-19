2be59cf3f43eb5edd2e6bd0da7317853682480a6
hyuntae <believe4279@gmail.com>
2021-08-19 16:47:25 +0900

    ex00

---
+++ fe00/ex00/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">ToDoList</h1>
+      <section className="리스트입력">입력할 부분</section>
+      <section className="리스트출력">할일 리스트 부분</section>
+    </div>
+  );
+}
+
+export default App;

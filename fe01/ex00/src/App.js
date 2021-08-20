105ca3efa46f914ca7bd9ca9fe39792fe0aaad06
Hyeonji-Jung <welloff.jj@gmail.com>
2021-08-20 15:32:21 +0900

    0820

---
+++ fe01/ex00/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+	
+	state = {
+		input: ''
+	}
+
+  	return (
+  	);
+}
+
+export default App;
---
+++ fe01/ex00/src/input.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+
+    render() {
+        
+    }
+}

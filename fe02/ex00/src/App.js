1d8fb1e8828b013eca2ddb5a273bcd7d041831e3
Hyeonji-Jung <welloff.jj@gmail.com>
2021-08-21 22:33:54 +0900

    0821

--- fe02/ex00/src/App.js
+++ fe02/ex00/src/App.js
@@ -21,18 +21,18 @@ const App = () => {
 		}
 		getUser();
 	}, []);
-	// console.log(data);
+//	console.log(data);
 
 	return (
 		<div>
 			{data ? data.map((data) => (
 				<ul>
 					{data.avatar_url ?
-						<img src={data.avatar_url} alt="profile" key={data.id}></img>
-						: '프로필 사진이 존재하지 않습니다.' }
-					<li key={`${data.id} + 1`}>유저명: {data.name}</li>
-					<li key={`${data.id} + 2`}>e-mail: {data.person? data.person.email : 'no E-mail'}</li>
-					<li key={`${data.id} + 3`}>type: {data.type}</li>
+						<img src={data.avatar_url} alt="profile" key={`${data.id}0`}></img>
+						: <div></div> }
+					<li key={`${data.id}1`}>유저명: {data.name}</li>
+					<li key={`${data.id}2`}>e-mail: {data.person? data.person.email : 'e-mail 정보가 존재하지 않습니다.'}</li>
+					<li key={`${data.id}3`}>type: {data.type}</li>
 				</ul>
 			)) : '유저 정보가 없습니다.'}
 		</div>

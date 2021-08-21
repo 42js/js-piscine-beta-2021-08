27bbb92e16bcf635ab4258e0a157d9807e2e1d2b
kunlee <leekh716@naver.com>
2021-08-21 19:44:22 +0900

    fe02 fin

--- fe02/ex00/src/App.js
+++ fe02/ex00/src/App.js
@@ -1,34 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios'
-import GetInfo from './GetInfo';
 
 const App = () => {
-	// const final = axios.create({
-	// 	headers: {
-	// 		"Notion-Version": "2021-08-16",
-	// 	},
-	// })
+	const [data, setData] = useState(null);
+	const [error, setError] = useState(null);
 
-	// final.defaults.headers.common.Authorization = `Bearer ${process.env.REACT_APP_NOTION_TOKEN}`;
-	// const URL = 'https://api.notion.com/v1/users';
-
-	// useEffect(() => {
-	// 		try {
-	// 			final.get(URL)
-	// 				.then((response) => {
-	// 					console.log(response)
-	// 			})
-	// 		}
-	// 		catch (e) {
-	// 			console.log(e)
-	// 		}
-	// }, [])
+	useEffect(() => {
+		const getApi = async () => {
+			const url = `https://cors.bridged.cc/https://api.notion.com/v1/users/`;
+			try {
+				const response = await axios.get(url, {
+					headers: {
+						"Authorization": `Bearer ${process.env.REACT_APP_NOTION_TOKEN}`,
+						"Notion-Version": "2021-08-16",
+					}
+				})
+				setData(response.data.results)
+				console.log(response.data)
+			} catch (e) {
+				setError(e);
+			}
+		}
+		getApi();
+	}, []);
 
 	return (
 		<div>
-			<GetInfo />
-			hi
+			{error ? error : (data ? data.map((data) => (
+				<ul>
+					<li key={data.id}>{data.name}</li>
+					<li key={`${data.id} + 1`}>{data.person? data.person.email : 'no E-mail'}</li>
+					<li key={`${data.id} + 2`}>{data.type}</li>
+					<img alt={data.name}>{data.avatar_url}</img>
+				</ul>
+			)) : 'No Users!')}
 		</div>
 	)
 

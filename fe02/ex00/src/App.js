05add65c1d0a5ae17ada0d0701a41dfb333e939b
Hyeonji-Jung <welloff.jj@gmail.com>
2021-08-21 22:20:38 +0900

    0821

--- fe02/ex00/src/App.js
+++ fe02/ex00/src/App.js
@@ -1,24 +1,42 @@
-const { Client } = require("@notionhq/client");
-const axios = require("axios");
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const notion = new Client({
-    auth: process.env.REACT_APP_NOTION_ACCESS_TOKEN,
-})
+const App = () => {
+	const [data, setData] = useState(null);
 
-const list = (async () => {
-	console.log(notion);
-  	const listUsersResponse = await axios({
+	useEffect(() => {
+		const getUser = async () => {
+			try {
+				const response = await axios.get("https://cors-anywhere.herokuapp.com/https://api.notion.com/v1/users", {
+					headers: {
+						"Authorization": `Bearer ${process.env.REACT_APP_NOTION_ACCESS_TOKEN}`,
+						"Notion-Version": "2021-08-16",
+					}
+				});
+				setData(response.data.results);
+			}
+			catch (error) {
+				console.log(error);
+			}
+		}
+		getUser();
+	}, []);
+	// console.log(data);
 
-	});
-  	// console.log(listUsersResponse);
-})
-
-function App() {
-  	list();
-  	return (
-    	<div>
-    	</div>
-  	);
+	return (
+		<div>
+			{data ? data.map((data) => (
+				<ul>
+					{data.avatar_url ?
+						<img src={data.avatar_url} alt="profile" key={data.id}></img>
+						: '프로필 사진이 존재하지 않습니다.' }
+					<li key={`${data.id} + 1`}>유저명: {data.name}</li>
+					<li key={`${data.id} + 2`}>e-mail: {data.person? data.person.email : 'no E-mail'}</li>
+					<li key={`${data.id} + 3`}>type: {data.type}</li>
+				</ul>
+			)) : '유저 정보가 없습니다.'}
+		</div>
+	)
 }
 
 export default App;

4817851c2b3b16a8de549498cbb9571df629a996
gilee <gilee@student.42seoul.kr>
2021-08-21 23:37:02 +0900

    [FE02] gilee 6일차

--- fe02/ex00/backend/routes/index.js
+++ fe02/ex00/backend/routes/index.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 var express = require('express');
 var router = express.Router();
 const { Client } = require("@notionhq/client");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const notion = new Client({
-    auth: 'secret_du5aohxokedsyRy3cCVt3vJYsuSDaervcFkMAp0DVmB',
+    auth: process.env.SECRET,
   });
   ;(async () => {
     const listUsersResponse = await notion.users.list()

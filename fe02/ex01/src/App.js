2e359cef482f05ff3ccc88373ae839e1a7ee7f65
Hyeonji-Jung <welloff.jj@gmail.com>
2021-08-21 23:29:45 +0900

    0821

--- fe02/ex01/src/App.js
+++ fe02/ex01/src/App.js
@@ -5,28 +5,31 @@ const App = () => {
 	const [data, setData] = useState(null);
 
 	useEffect(() => {
-		const getUser = async () => {
+		const getList = async () => {
 			try {
-				const response = await axios.get("https://cors-anywhere.herokuapp.com/https://api.notion.com/v1/users", {
+				console.log("요청!");
+				const response = await axios.get("https://cors-anywhere.herokuapp.com/https://api.notion.com/v1/search", {
 					headers: {
 						"Authorization": `Bearer ${process.env.REACT_APP_NOTION_ACCESS_TOKEN}`,
 						"Notion-Version": "2021-08-16",
-					}
+					},
+					page_size: 30
 				});
 				setData(response.data.results);
+				// console.log(response.data.results);
 			}
 			catch (error) {
 				console.log(error);
 			}
 		}
-		getUser();
+		getList();
 	}, []);
 
-//	console.log(data);
+	console.log(data);
 
 	return (
 		<div>
-			{data ? data.map((data) => (
+			{/* {data ? data.map((data) => (
 				<ul>
 					{data.avatar_url ?
 						<img src={data.avatar_url} alt="profile" key={`${data.id}0`}></img>
@@ -35,7 +38,7 @@ const App = () => {
 					<li key={`${data.id}2`}>e-mail: {data.person? data.person.email : 'e-mail 정보가 존재하지 않습니다.'}</li>
 					<li key={`${data.id}3`}>type: {data.type}</li>
 				</ul>
-			)) : '유저 정보가 없습니다.'}
+			)) : '유저 정보가 없습니다.'} */}
 		</div>
 	)
 }

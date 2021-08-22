c5b7a3f5cfe4da66e444b1ef545d973e44e1aa01
from97 <skm1104@daum.net>
2021-08-22 09:23:29 +0900

    eslint

--- fe03/issue-tracker/src/App.js
+++ fe03/issue-tracker/src/App.js
@@ -1,10 +1,7 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;

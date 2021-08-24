2300908398aad5c5301e48e95d2574438b35f198
Sungyong Cho <to.sungyongcho@gmail.com>
2021-08-25 04:48:20 +0900

    ex00 fix

--- be00/ex00/dist/index.js
+++ be00/ex00/dist/index.js
@@ -4,14 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
-const output = '안녕, Node.js!';
+const port = 4242;
 const server = http_1.default.createServer((request, response) => {
+    const output = '안녕, Node.js!';
     response.writeHead(200, {
         'Content-Type': 'text',
         'Content-Length': output.length,
     });
     response.end(output);
 });
-server.listen(4242, () => {
-    console.log('Server running at http://localhost:4242/');
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
 });
--- be00/ex00/index.ts
+++ be00/ex00/index.ts
@@ -1,7 +1,8 @@
 import http from 'http';
 
-const output = '안녕, Node.js!';
+const port = 4242;
 const server = http.createServer((request, response) => {
+  const output = '안녕, Node.js!';
   response.writeHead(200, {
     'Content-Type': 'text',
     'Content-Length': output.length,
@@ -9,6 +10,6 @@ const server = http.createServer((request, response) => {
   response.end(output);
 });
 
-server.listen(4242, () => {
-  console.log('Server running at http://localhost:4242/');
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });

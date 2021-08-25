a088779113cf70d9d4a326b618dee8c949e19689
Yenowme <76904042+Yenowme@users.noreply.github.com>
2021-08-25 14:31:06 +0900

    ex00: create server by http module

---
+++ be00/ex00/ex00.js
@@ -0,0 +1,8 @@
+const http = require("http");
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+    res.write("안녕, Node.js!");
+    res.end();
+}).listen(4242);
+console.log("Server running at http://localhost:4242/");

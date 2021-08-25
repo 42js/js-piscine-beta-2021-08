5b014188e126ac0975ebad71afb6852648536ef7
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-25 23:53:09 +0900

    http.get 부분 삭제

--- be00/ex00/ex00.js
+++ be00/ex00/ex00.js
@@ -1,52 +1,28 @@
-import http, { createServer } from "http";
+import { createServer } from "http";
 import console from "console";
 
-const SCHEME = "http";
-const HOSTNAME = "localhost";
-const PORT = 4242;
-
 // webserver object 생성
-const server = createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-  res.write(JSON.stringify("안녕, Node.js!")); // content 내보내기: body 부분의 content: ;
-  res.end(); // content 출력 완료(응답 종료)
+// 이 server로 오는 HTTP 요청마다 createServer에 전달된 함수가 한번씩 호출됨
+createServer((request, response) => {
+  const { headers, method, url } = request;
+  let body = [];
+
+  request //
+    .on("error", (err) => console.log(err))
+    .on("data", (chunk) => body.push(chunk))
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+      console.log(method);
+      console.log(url);
+      console.log(headers);
+      console.log(body);
+      response.writeHead(200, {
+        "Content-Type": "text/plain; charset=utf-8",
+        Connection: "close",
+      });
+      response.write("안녕, Node.js!"); // content 내보내기: body 부분의 content
+      response.end(); // content 출력 완료(응답 종료)
+    });
 }).listen(4242, () => {
-  console.log(`Server is running at ${SCHEME}://${HOSTNAME}:${PORT}/`);
+  console.log(`Server is running at http://localhost:4242/`);
 });
-
-http
-  .get("http://localhost:4242", (res) => {
-    const { statusCode } = res;
-    const contentType = res.headers["content-type"];
-
-    let error;
-    if (statusCode !== 200) {
-      error = new Error(`Request Failed.\nStatus Code: ${statusCode}`);
-    } else if (!/^text\/plain/.test(contentType)) {
-      error = new Error(
-        `Invalid content-type.\nExpected text/html but received ${contentType}`
-      );
-    }
-    if (error) {
-      console.error(error.message);
-      res.resume();
-      return;
-    }
-
-    res.setEncoding("utf8");
-    let rawData = "";
-    res.on("data", (chunk) => {
-      rawData += chunk;
-    });
-    res.on("end", () => {
-      try {
-        const parsedData = JSON.parse(rawData);
-        console.log(parsedData);
-      } catch (e) {
-        console.error(e.message);
-      }
-    });
-  })
-  .on("error", (e) => {
-    console.error(`Got error: ${e.message}`);
-  });

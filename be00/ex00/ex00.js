5702074ef123171966fde94139b26dafa701f98c
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-25 15:10:08 +0900

    ex00 test 확인

--- be00/ex00/ex00.js
+++ be00/ex00/ex00.js
@@ -1,44 +1,52 @@
-import http, { createServer } from 'http';
-import url from 'url';
-import console from 'console';
+import http, { createServer } from "http";
+import console from "console";
 
-const options = {
-  hostname: 'localhost',
-  port: '4242',
-};
+const SCHEME = "http";
+const HOSTNAME = "localhost";
+const PORT = 4242;
 
-http.get('http://localhost:4242', options, (res) => {
-  const { statusCode } = res;
-  const contentType = res.headers['content-type'];
-
-  let error;
-  if (statusCode !== 200) {
-    error = new Error(`Request Failed.\nStatus Code: ${statusCode}`);
-  } else if (!/^text\/html/.test(contentType)) {
-    error = new Error(`Invalid content-type.\nExpected text/html but received ${contentType}`);
-  }
-  if (error) {
-    console.error(error.message);
-    res.resume();
-    return;
-  }
-
-  res.setEncoding('utf8');
-  let rawData = '';
-  res.on('data', (chunk) => { rawData += chunk; });
-  res.on('end', () => console.log(`Server is running on ${res.url}`));
-}).on('error', (e) => {
-  console.error(`Got error: ${e.message}`);
-});
-
+// webserver object 생성
 const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<html lang="en">');
-  res.write('<head><meta charset="UTF-8"><title>안녕, Node.js!</title></head>');
-  res.write('<body><h1>안녕, Node.js!</h1></body>');
-  res.write('</html>');
+  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+  res.write(JSON.stringify("안녕, Node.js!")); // content 내보내기: body 부분의 content: ;
+  res.end(); // content 출력 완료(응답 종료)
+}).listen(4242, () => {
+  console.log(`Server is running at ${SCHEME}://${HOSTNAME}:${PORT}`);
 });
 
-server.listen(4242, () => {
-  console.log(`Server is running on ${url}`);
-});
+http
+  .get("http://localhost:4242", (res) => {
+    const { statusCode } = res;
+    const contentType = res.headers["content-type"];
+
+    let error;
+    if (statusCode !== 200) {
+      error = new Error(`Request Failed.\nStatus Code: ${statusCode}`);
+    } else if (!/^text\/plain/.test(contentType)) {
+      error = new Error(
+        `Invalid content-type.\nExpected text/html but received ${contentType}`
+      );
+    }
+    if (error) {
+      console.error(error.message);
+      res.resume();
+      return;
+    }
+
+    res.setEncoding("utf8");
+    let rawData = "";
+    res.on("data", (chunk) => {
+      rawData += chunk;
+    });
+    res.on("end", () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+        console.log(parsedData);
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
+  })
+  .on("error", (e) => {
+    console.error(`Got error: ${e.message}`);
+  });

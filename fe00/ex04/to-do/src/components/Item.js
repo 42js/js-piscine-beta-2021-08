f553cd67d5237012adf49db2f2069ae07aa63937
jikwon <jikwon@student.42seoul.kr>
2021-08-26 05:59:41 +0900

    ex04

--- fe00/ex04/to-do/src/components/Item.js
+++ fe00/ex04/to-do/src/components/Item.js
@@ -7,14 +7,14 @@ class Item extends Component {
        
         return (
             <div className="todo-item" onClick={()=>onToggle(id)}>
- 
+  				{
+                   checked && (<div className="check-mark">✔️</div>)
+                }
+
                 <div className={`todo-text ${checked && 'checked'}`}> 
                         <div>{text}</div>
                 </div>
-                {
-                   checked && (<div className="check-mark">✔️</div>)
-                }
-                <div className="remove" onClick={(e)=>{
+                               <div className="remove" onClick={(e)=>{
                     e.stopPropagation();
                     onRemove(id);
                 }}>&times;</div>

314544bbf4055442e403d012fb5c8e77fded4f6b
jikwon <jikwon@student.42seoul.kr>
2021-08-25 23:45:53 +0900

    ex01

--- fe00/ex01/to-do/src/index.js
+++ fe00/ex01/to-do/src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';			// jsx 문법을 사용하기 위함	
-import ReactDOM from 'react-dom';	// react앱을 최초 렌더링 하기 위함
-import './index.css';				
-import App from './App';			// app = react component 
-import reportWebVitals from './reportWebVitals';	// 오프라인 또는 네트워크가 좋지 않는 상태에서도 온라인인 것처럼 리소스들을 보여주는 모듈
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// punlic/indext.html 파일을 기준으로 id=root 태그를 찾아 app을 렌더링시킴.
 ReactDOM.render(
   <React.StrictMode>
     <App />

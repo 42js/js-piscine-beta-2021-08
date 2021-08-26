48ddfd5654bd7b8fce7679381459d234c931677e
Sungyong Cho <to.sungyongcho@gmail.com>
2021-08-27 00:45:56 +0900

    hi

---
+++ be01/ex00/src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const { sequelize } = require('./models/index');
+
+const app = express();
+const port = 4242;
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database connected successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+app.listen(port, () => { });

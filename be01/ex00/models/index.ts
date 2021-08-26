10d809e13cda2dd36a03cd4297c4a4795956c348
Sungyong Cho <to.sungyongcho@gmail.com>
2021-08-27 01:29:56 +0900

    좀만쉬자

--- be01/ex00/models/index.ts
+++ be01/ex00/models/index.ts
@@ -4,10 +4,10 @@ import Sequelize from 'sequelize';
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = `${__dirname}/../config/config.json`[env];
+const config = require(`${__dirname}/../config/config.json`)[env];
 const db = { };
 
-let sequelize = Sequelize;
+let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {

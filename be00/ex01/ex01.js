52130eb273e3e2b996d264a0b7b3af76672136b7
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-26 12:45:05 +0900

    readFile로 다시 수정

--- be00/ex01/ex01.js
+++ be00/ex01/ex01.js
@@ -1,15 +1,21 @@
 import { createServer } from "http";
-import fs from "fs/promises";
+import fs from "fs";
 
 createServer(async (req, res) => {
-  try {
-    const data = await fs.readFile("./ex01.html");
-    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-    res.end(data);
-  } catch (err) {
-    console.log(err);
-    res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
-    res.end(err.message);
+  if (req.method === "GET") {
+    fs.readFile("./ex01.html", (err, data) => {
+      if (err) {
+        console.log(err);
+        res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
+        res.end(err.message);
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(405, { "Content-Type": "text/html; charset=utf-8" });
+    res.end("<h1>405 Method Not Allowed</h1>");
   }
 }).listen(4242, () => {
   console.log("Server running at http://localhost:4242/");

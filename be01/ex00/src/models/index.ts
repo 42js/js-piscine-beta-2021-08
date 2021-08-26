e1f849afe7d9cc94158ec74be69204a7dabdfd30
Sungyong Cho <to.sungyongcho@gmail.com>
2021-08-27 03:29:24 +0900

    let's do

--- be01/ex00/src/models/index.ts
+++ be01/ex00/src/models/index.ts
@@ -4,10 +4,10 @@ import Sequelize from 'sequelize';
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.json`)[env];
+const config = JSON.parse(fs.readFileSync('../config/config.json'));
 const db = { };
 
-let sequelize;
+let sequelize: any;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {

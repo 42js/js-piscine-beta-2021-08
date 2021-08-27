e523934b04d4d1ae3d04682301a93e88cb9cec72
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2021-08-27 19:12:25 +0900

    fe02 init

---
+++ fe02/ex00/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+async function App() {
+
+
+  const getData = await axios.get('')
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;

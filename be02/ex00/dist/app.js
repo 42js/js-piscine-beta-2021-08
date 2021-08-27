55accd12440e65af8486e18df31462f6de49233d
Sungyong Cho <to.sungyongcho@gmail.com>
2021-08-28 01:22:47 +0900

    ex00 done

---
+++ be02/ex00/dist/app.js
@@ -0,0 +1,54 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv = __importStar(require("dotenv"));
+const express_1 = __importDefault(require("express"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const routes_1 = __importDefault(require("./routes/routes"));
+dotenv.config();
+const PORT = parseInt(process.env.PORT, 10);
+const HOST = String(process.env.HOST);
+const app = (0, express_1.default)();
+// app.use(
+//   session({
+//     // httpOnly: true, // 자바스크립트를 통해 세션 쿠키를 사용할 수 없도록 함
+//     // secure: true, // https 환경에서만 session 정보를 주고받도록처리
+//     secret: 'secret key', // 암호화하는 데 쓰일 키
+//     resave: false, // 세션을 언제나 저장할지 설정함
+//     saveUninitialized: true, // 세션이 저장되기 전 uninitialized 상태로 미리 만들어 저장
+//     cookie: {
+//       // 세션 쿠키 설정 (세션 관리 시 클라이언트에 보내는 쿠키)
+//       httpOnly: true,
+//       secure: true,
+//     },
+//   }),
+// );
+app.use(express_1.default.json());
+// what is this
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cookie_parser_1.default)());
+app.use(routes_1.default);
+app.listen(PORT, HOST, () => {
+    console.log(`Server Listening on http://${HOST}:${PORT}`);
+});

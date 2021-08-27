4c273c306f21b771761fbc5b52540d7d817ac2b0
Chloekkk <skdusdl8804@gmail.com>
2021-08-27 20:39:07 +0900

    ex01 : dotenv setting

--- be01/ex01/app.js
+++ be01/ex01/app.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const PORT = 4242;
 const {sequelize} = require('./models/index');
 
-sequelize.sync()
-	.then(() => {
-		console.log("Database connected successfully.");
-	})
-	.catch((err) => {
-		console.error(err);
-		console.log("Database connection error.");
-		process.exit();
-	});
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}.`);
-});
+sequelize
+    .sync()
+    .then(() => {
+        console.log("Database connected successfully.");
+    })
+    .catch((err) => {
+        console.error(err);
+        console.log("Database connection error.");
+        process.exit();
+    });
+app
+    .use(morgan("tiny"))
+    .listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}.`);
+    });

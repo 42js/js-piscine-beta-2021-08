fefd1b97ef1b4184a4e61c2e15fe768eeeaaaaf9
Chloekkk <skdusdl8804@gmail.com>
2021-08-27 20:07:41 +0900

    ex00 : update complete

--- be01/ex00/app.js
+++ be01/ex00/app.js
@@ -3,7 +3,12 @@ const app = express();
 const { sequelize } = require('./models'); // mySQL sequelize model
 const PORT = 4242;
 
-sequelize.sync(); // 서버가 실행될 때 seqeulize schema를 DB 에 적용시킨다
-app.listen(PORT, () => {
-	console.log("Server is running at http://localhost:4242");
-})
+sequelize.sync()
+	.then(() => {
+		console.log("Database connected successfully.");
+	})
+	.catch(err => {
+		console.error(err);
+		console.log("Database connection error.");
+		process.exit();
+	}); // 서버가 실행될 때 seqeulize schema를 DB 에 적용시킨다

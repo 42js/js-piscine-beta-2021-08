b40594149540f320f78bf4b72d0b170772cb8daa
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2021-08-28 13:29:11 +0900

    fe02 ing..

--- fe02/ex00/src/App.js
+++ fe02/ex00/src/App.js
@@ -3,13 +3,45 @@ import axios from 'axios';
 
 async function App() {
 
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const getData = await axios.get('')
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        setError(null);
+        setUser(null);
+        setLoading(true);
 
+        const getData = await axios.get("https://cors-anywhere.herokuapp.com/https://api.notion.com/v1/users", {
+					headers: {
+						"Authorization": `Bearer ${process.env.REACT_APP_NOTION_KEY}`,
+						"Notion-Version": "2021-08-16",
+					}
+				});
+				setUser(getData.data.results);
+      } catch (e)  {
+        setError(e);
+      }
+      setLoading(false);
+    };
+
+    fetchUser();
+  }, []);
+
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>Error: {error}</div>;
+  if (!user) return null;
   return (
-    <>
-    </>
-  );
+    <ul>
+      {user.map(user => (
+        <li key={user.id}>
+          {user.username} ({user.name})
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default App;
